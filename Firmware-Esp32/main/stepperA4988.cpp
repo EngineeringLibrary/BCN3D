0566842e6f09ae49a15426157e1e25dbc088d2ac
domingosliraneto <domingosliraneto@hotmail.com>
2017-11-07 15:29:08 -0300

    Calcular a trajetória para cada motor

--- Firmware-Esp32/main/stepperA4988.cpp
+++ Firmware-Esp32/main/stepperA4988.cpp
@@ -22,8 +22,37 @@ void stepperA4988::newStep(bool direction)
         this->direction = direction;
         gpio_set_level(this->direction_pin,this->direction);
       }
-      vTaskDelay(15 / portTICK_PERIOD_MS);
+      //vTaskDelay(15 / portTICK_PERIOD_MS);
       gpio_set_level(this->step_pin,0);
-      vTaskDelay(15 / portTICK_PERIOD_MS);
+      stepAcel(2400);
+      //vTaskDelay(15 / portTICK_PERIOD_MS);
       gpio_set_level(this->step_pin,1);
+      stepAcel(2400);
+}
+
+void stepAcel (int steps)
+{
+  int speedMax = 2000;
+  int speedMin = 100;
+  int MudSenti = 2;
+
+  int stopStepAcel = (speedMax - speedMin) / MudSenti;
+
+  if ( stopStepAcel > steps / 2 )
+    stopStepAcel = steps / 2;
+  int initStepAcel = steps - stopStepAcel;
+
+  int d = speedMax;
+
+  for( int i = 0; i < steps; ++i)
+  {
+    gpio_set_level(this->step_pin,1);
+    gpio_set_level(this->step_pin,0);
+    vTaskDelay(d);
+
+    if(i < stopStepAcel)
+      d -= MudSenti;
+    else if(i < stopStepAcel)
+      d += MudSenti;
+  }
 }
