28ac9d6a0bb91a4e6e518f165810d1ec413dad32
jvgalvao <galvaogalvao3@hotmail.com>
2017-11-13 08:18:30 -0300

    threads implementation

--- Firmware-Esp32/main/robotica_main.cpp
+++ Firmware-Esp32/main/robotica_main.cpp
@@ -14,25 +14,74 @@
 #include "esp_spi_flash.h"
 #include "stepperA4988.h"
 #include "servomotor.h"
+ 
+ //Variavel Global
+int step1, step2, step3, step4, step5;
 
 gpio_num_t stepPinMotor1 = GPIO_NUM_22, directionPinMotor1 = GPIO_NUM_23, servoPin = GPIO_NUM_21;
-stepperA4988 motor1(stepPinMotor1, directionPinMotor1);
+stepperA4988 motor1(stepPinMotor1, directionPinMotor1),
+stepperA4988 motor2(stepPinMotor2, directionPinMotor2),
+stepperA4988 motor3(stepPinMotor3, directionPinMotor3),
+stepperA4988 motor4(stepPinMotor4, directionPinMotor4),
+stepperA4988 motor5(stepPinMotor5, directionPinMotor5);
 servomotor garra(servoPin);
 
-void controlMotor(void*arg)
+//Função para controlar o servo motor
+void controlMotorGarra(void*arg)
 {
     while(1)
         garra.setPosition(500);
 }
 
-void stepControlMotor(void*arg)
-{
-    while(1)
-        motor1.newStep();
-}
+//Funçãos para controlar os motores de passo
+void stepControlMotor01(void*arg)
 
+  for(int i = 0; i < step1; ++i){
+                motor1.newStep();
+      }
+
+void stepControlMotor02(void*arg)
+  
+  for(int i = 0; i < step2; ++i){
+                motor2.newStep();
+      }
+
+void stepControlMotor03(void*arg)
+
+  for(int i = 0; i < step3; ++i){
+                motor3.newStep();
+      }
+void stepControlMotor04(void*arg)
+
+  for(int i = 0; i < step4; ++i){
+                motor4.newStep();
+      }
+
+void stepControlMotor05(void*arg)
+
+  for(int i = 0; i < step5; ++i){
+                motor5.newStep();
+      }
+
+
+void movimento(){
+
+      }
+
+//Cadastro das funções que vão trabalhar em paralelo
 extern "C" void app_main()
-{
-    xTaskCreate(controlMotor, "controlMotor", 1024 * 2, NULL, 5, NULL);
-    xTaskCreate(stepControlMotor, "step", 1024 * 2, NULL, 5, NULL);
+{   
+    xTaskCreate(controlMotor, "controlMotorGarra", 1024 * 2, NULL, 5, NULL);
+    
+    while( true){
+         xTaskCreate(stepControlMotor01, "stepControlMotor01", 1024 * 2, NULL, 5, NULL);
+         xTaskCreate(stepControlMotor02, "stepControlMotor02", 1024 * 2, NULL, 5, NULL);
+         xTaskCreate(stepControlMotor03, "stepControlMotor03", 1024 * 2, NULL, 5, NULL);
+         xTaskCreate(stepControlMotor04, "stepControlMotor04", 1024 * 2, NULL, 5, NULL);
+         xTaskCreate(stepControlMotor05, "stepControlMotor05", 1024 * 2, NULL, 5, NULL); 
+
+      sdk_os_delay_ms(10);
+    }
+
+  
 }
