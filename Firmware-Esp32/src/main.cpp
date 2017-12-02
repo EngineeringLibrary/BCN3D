42dee3d9da76f63a0e21c2ef9d3ae7dfca34c815
Domingos Neto <domingosliraneto@hotmail.com>
2017-12-02 16:46:36 -0200

    Melhoria na main, e adicionado comentarios

--- Firmware-Esp32/src/main.cpp
+++ Firmware-Esp32/src/main.cpp
@@ -20,31 +20,23 @@
 #include <typeinfo>
 #include <algorithm>
 
+//Está função aguarda o recebimento de dados wifi, caso tenha dados ela será executada.
 void testeReceberWifi(const char* dadosRecebidos){
 
   M = dadosRecebidos;
 
-   for(linha = 0; linha < M.getNumberOfRows(); ++linha ){
+   for(linha = 0; linha < M.getNumberOfRows(); ++linha){
      stepControlMotor01();
      stepControlMotor02();
      stepControlMotor03();
      stepControlMotor04();
      stepControlMotor05();
-     // vTaskDelay(1000 / portTICK_PERIOD_MS);
    }
+   linha = 0;
 }
 
 //Cadastro das funções que vão trabalhar em paralelo
 extern "C" void app_main()
 {
      wifi_TCP_server_init(testeReceberWifi);
-
-    // xTaskCreate(stepControlMotor01, "stepControlMotor01", 1024*2, NULL, 5, NULL);
-    // xTaskCreate(stepControlMotor02, "stepControlMotor02", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    // xTaskCreate(stepControlMotor03, "stepControlMotor03", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    // xTaskCreate(stepControlMotor04, "stepControlMotor04", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    // xTaskCreate(stepControlMotor05, "stepControlMotor05", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-    // xTaskCreate(controlMotorGarra, "controlMotorGarra", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-
 }
